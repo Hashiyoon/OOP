79bc1f362e5dfb9d60e391c268ff4478ae55510e
Maks <129111791+GuseMs@users.noreply.github.com>
2025-12-20 18:33:32 +0300

    Delete src/main directory

--- src/main/main/java/org/example/server/servlets/ProfileServlet.java
+++
@@ -1,7 +0,0 @@
-package org.example.server.servlets;
-
-public class ProfileServlet {
-    public ProfileServlet() {
-        System.out.println("Сервлет пошёл в БД за данными пользователя");
-    }
-}
